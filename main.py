8109ff7d47ecd34d7a7daeb9c457d356b6ba80ac
GS-bit <gabriel.soares.lx@gmail.com>
2026-06-03 15:31:09 -0300

    Second commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import sys
+
+from yolo import Yolo
+
+def read_line() -> tuple:
+    """
+    Lê a imagem da fila do RU, cuja localização é passada como argumento do programa 
+    (python main.py [argumento]), e retorna uma tupla com dados da fila. O primeiro elemento dessa tupla
+    é o número de pessoas na fila e o segundo é o tempo de espera. A função retorna None em caso de erro.
+    """
+
+    if len(sys.argv) == 1: # A localização da imagem não foi passada
+        print("Imagem não informada")
+        return None
+
+    try:
+        print("Lendo", sys.argv[1])
+
+        yolo = Yolo()
+        yolo.read(sys.argv[1])
+        yolo.save()
+
+        people_in_line = yolo.get_num_of_people()
+        waiting_time = 0
+
+        return (people_in_line, waiting_time)
+    except:
+        return None
+
+
+if __name__ == "__main__":
+    print("Retorno da função read_line:", read_line())
---
+++ yolo.py
@@ -0,0 +1,75 @@
+import cv2
+from ultralytics import YOLO as YOLO_MODEL
+
+class Yolo:
+    def __init__(self):
+        self.model = YOLO_MODEL("yolov8n.pt") # Modelo
+
+        self.filepath = None
+        self.image = None
+        self.results = None
+
+    def read(self, filepath) -> int:
+        """
+        Esta função modifica as variáveis da classe, atualizando seus valores conforme
+        o processamento da imagem passada como argumento.
+
+        Argumentos:
+            filepath: localização da imagem
+
+        Retorno:
+            0 em caso de sucesso,
+            1 em caso de erro
+        """
+
+        try:
+            self.filepath = filepath
+            self.image = cv2.imread(filepath)
+            self.results = self.model(self.image, classes=0) # "Classes" igual a 0 significa detecção de apenas pessoas
+
+            return 0
+        except:
+            return 1
+
+    def save(self) -> int:
+        """
+        Esta função salva a foto processada pela função self.read, com as caixas de identificação de pessoas.
+        O local de salvamento do arquivo é o mesmo onde está a foto original, mas com o prefixo "MODIFIED_".
+
+        Retorno:
+            0 em caso de sucesso,
+            1 em caso de erro
+        """
+                
+        try:
+            for result in self.results:
+                boxes = result.boxes
+                
+                for box in boxes:
+                    x1, y1, x2, y2 = map(int, box.xyxy[0])
+                    
+                    cv2.rectangle(self.image, (x1, y1), (x2, y2), (255, 0, 80), 2)
+
+            output_path = f"MODIFIED_{self.filepath}"
+            cv2.imwrite(output_path, self.image)
+            print(f"Imagem salva em {output_path}")
+        except:
+            return 1
+        
+    def get_num_of_people(self):
+        """
+        Retorna o número de pessoas encontradas na imagem.
+        """
+
+        try:
+            count = 0
+            for result in self.results:
+                boxes = result.boxes
+                
+                for box in boxes:
+                    count += 1
+
+            return count
+        except:
+            return 0
+

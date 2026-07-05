8fefe75bac34b8eb2202f69dc102caae1e24bed8
Tomas Kavela <javelamanuel@gmail.com>
2026-07-05 16:22:17 +0100

    Change YOLO model to use 'yolov8s.pt'

--- yolo.py
+++ yolo.py
@@ -2,9 +2,9 @@ import cv2
 from ultralytics import YOLO as YOLO_MODEL
 
 class Yolo:
-    def __init__(self):
-        self.model = YOLO_MODEL("yolov8x.pt") # Modelo
 
+    def __init__(self):
+        self.model = YOLO_MODEL("yolov8s.pt")  # Modelo small — seguro para 512MB RAM
         self.filepath = None
         self.image = None
         self.results = None
@@ -21,12 +21,10 @@ class Yolo:
             0 em caso de sucesso,
             1 em caso de erro
         """
-
         try:
             self.filepath = filepath
             self.image = cv2.imread(filepath)
-            self.results = self.model(self.image, classes=0) # "Classes" igual a 0 significa detecção de apenas pessoas
-
+            self.results = self.model(self.image, classes=0)  # "Classes" igual a 0 significa detecção de apenas pessoas
             return 0
         except:
             return 1
@@ -40,14 +38,11 @@ class Yolo:
             0 em caso de sucesso,
             1 em caso de erro
         """
-                
         try:
             for result in self.results:
                 boxes = result.boxes
-                
                 for box in boxes:
                     x1, y1, x2, y2 = map(int, box.xyxy[0])
-                    
                     cv2.rectangle(self.image, (x1, y1), (x2, y2), (255, 0, 80), 2)
 
             output_path = f"MODIFIED_{self.filepath}"
@@ -55,21 +50,17 @@ class Yolo:
             print(f"Imagem salva em {output_path}")
         except:
             return 1
-        
+
     def get_num_of_people(self) -> int:
         """
         Retorna o número de pessoas encontradas na imagem.
         """
-
         try:
             count = 0
             for result in self.results:
                 boxes = result.boxes
-                
                 for box in boxes:
                     count += 1
-
             return count
         except:
             return 0
-
